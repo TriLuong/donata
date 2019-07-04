aea9d7cb438c169c2549417fddc6f776285ede7f
Tri Luong <lxt0094@gmail.com>
2019-07-04 17:32:18 +0700

    Modify footer display on Desktop.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -7,49 +7,51 @@
             <div class="col-md-6">
                 <p>Sed vulputate lorem ut bibendum elementum. Phasellus placerat elit arcu, at imperdiet augue ullamcorper id. Maecenas malesuada leo libero. </p>
             </div>
+           
+            <div class="row footerImg">
+                <div class="col-md-4">
+                    <img src="../assets/02-Lifestyle-photos/Donata_13Mar197766.jpg"/>
+                </div>
+                <div class="col-md-4">
+                    <img src="../assets/02-Lifestyle-photos/Donata_13Mar197838.jpg"/>
+                </div>
+                <div class="col-md-4">
+                    <img src="../assets/02-Lifestyle-photos/Donata_13Mar197791.jpg"/>
+                </div>
+            </div>
         </div>
 
         
 
         <div class="row footerBottom">
-            <div class="container-fluid">
-                <div class="row footerImg">
-                    <div class="col-md-4">
-                        <img src="../assets/02-Lifestyle-photos/Donata_13Mar197766.jpg"/>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="../assets/02-Lifestyle-photos/Donata_13Mar197838.jpg"/>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="../assets/02-Lifestyle-photos/Donata_13Mar197791.jpg"/>
-                    </div>
+            <div class="row footerBottomContact">
+                <div class="col-md-4 logo">
+                    <a href="#"><img src="../assets/logo.svg"/></a>
                 </div>
-                <div class="row footerBottomContact">
-                    <div class="col-md-4 logo">
-                        <a href="#"><img src="../assets/logo.svg"/></a>
-                    </div>
-                    <div class="col-md-8 info">
-                        <div class="row">
-                            <ul class="col-md-4">
-                                <h6>Contact</h6>
-                                <li>+84 937 991 235</li>
-                                <li>malene@donata-inc.com</li>
-                                <li>www.donata-inc.com</li>
-                            </ul>
-                            <ul class="col-md-4">
-                                <h6>Address</h6>
-                                <li>198C1 Nguyen Van Huong, Thao Dien, District 2, Ho Chi Minh City</li>
-                            </ul>
-                            <ul class="col-md-4">
-                                <h6>working time</h6>
-                                <li>Monday to Friday
-                                (9AM to 5PM). By appointment only.</li>
-                                <li>Please call our office to make an appointment</li>
+                <div class="col-md-8 info">
+                    <div class="row">
+                        <ul class="col-md-4">
+                            <h6>Contact</h6>
+                            <li>+84 937 991 235</li>
+                            <li>malene@donata-inc.com</li>
+                            <li>www.donata-inc.com</li>
+                        </ul>
+                        <ul class="col-md-4">
+                            <h6>Address</h6>
+                            <li>198C1 Nguyen Van Huong, Thao Dien, District 2, Ho Chi Minh City</li>
+                        </ul>
+                        <ul class="col-md-4">
+                            <h6>working time</h6>
+                            <li>Monday to Friday
+                            (9AM to 5PM). By appointment only.</li>
+                            <li>Please call our office to make an appointment</li>
 
-                            </ul>
-                        </div>
+                        </ul>
                     </div>
                 </div>
+            </div>
+
+            <div class="container-fluid">
                 <div class="row footerBottomCopyright">
                     <div class="col-md-6 left">
                         <p>&copy; 2019 Donata Inc.</p>
@@ -60,6 +62,7 @@
                 </div>
             </div>
         </div>
+        
         
         
     </footer>
@@ -76,28 +79,24 @@ export default {
         padding: 0;
         height: 100vh;
         min-height: 600px;
-        position: relative;
         .footerTop {
+            position: relative;
             background-color: black;
             color: white;
-            padding: 3rem 3rem 15rem;
+            padding: 3rem 3rem;
+            height: 300px;
             h1 {
                 text-transform: uppercase;
             }
             p{
                 text-align: left;
             }
-        }
-        
-        .footerBottom {
-            margin: 12rem 3rem 0rem !important;
-            position: absolute;
-            top:0;
-            left:0;
-            right:0;
+            p:last-child{
+                padding-left: 30px;
+            }
             .footerImg{
-                left:0;
-                right: 0;
+                position: absolute;
+                top: 190px;
                 img {
                     object-fit: cover;
                     width:250px;
@@ -110,10 +109,14 @@ export default {
                     text-align: right;
                 }
             }
+        }
+        
+        .footerBottom {
+            margin: 0 30px !important;
             .footerBottomContact{
                 border-bottom: 1px solid gainsboro;
                 padding-bottom: 5rem;
-                margin-top:5rem !important;
+                margin-top:12rem !important;
                 .logo {
                     text-align: left;
                     img {
