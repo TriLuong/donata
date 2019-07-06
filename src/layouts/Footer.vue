5acd97359d3dba750ab41905278921381115047f
Tri Luong <lxt0094@gmail.com>
2019-07-06 12:24:11 +0700

    Modify margin for footerImg.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -253,7 +253,7 @@ export default {
                         }
                         .footerImgResponsive{
                             display: block;
-                            margin-top:10px;
+                            margin-top:10px 0;
                         }
                     }
                 }
